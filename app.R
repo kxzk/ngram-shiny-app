32b57dc09fc05047d0c4fec41e5a604736669517
kade killary <kadekillary@protonmail.com>
2017-10-09 10:03:21 -0700

    re-did table as dataTable

--- app.R
+++ app.R
@@ -2,102 +2,84 @@ library(shiny)
 library(tidytext)
 library(shinythemes)
 library(tidyverse)
+library(DT)
 
-ui <- fluidPage(theme = shinytheme("darkly"),
+ui <- fluidPage(theme = shinytheme("simplex"),
 
   titlePanel("AdWords N-gram Analysis"),
   # Sidebar layout with input and output definitions
   sidebarLayout(
     # Sidebar panel for inputs
     sidebarPanel(
+      h4('Before you upload!'),
+      p('Read the', a(href='https://github.com/beigebrucewayne/NgramShinyApp/blob/master/README.md', 'data cleaning'), 'section.'),
+      hr(),
       # Input -> Select a file
       fileInput("file1", "Choose CSV File",
                 multiple = TRUE,
                 accept = c("text/csv",
                          "text/comma-separated-values,text/plain",
                          ".csv")),
-      # Horizontal line
       hr(),
-      # Input -> Checkbox if file has header
-      checkboxInput("header", "Header", TRUE),
-      # Input -> Select separator
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-      # Horizontal line
+      sliderInput('ngramCount', '# of Grams', min = 1, max = 6, value = 1),
       hr(),
-      # Input -> Select number of rows to display
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               Top50 = "top50"),
-                   selected = "head"),
-      hr(),
-      sliderInput('ngramCount', '# of Grams', min = 1, max = 6, value = 1)
+      p('Having trouble?'),
+      p('Email: kade.killary@xmedia.com')
     ),
     # Main panel for displaying outputs
     mainPanel(
       img(src='https://i.imgur.com/kB1qKI0.png', align = 'center'),
       br(),br(),
       h3('What\'s an N-gram?'),
-      p('An n-gram is a contiguous sequence of n items from a given sequence of text or speech', 
-        a(href='http://text-analytics101.rxnlp.com/2014/11/what-are-n-grams.html', ' - a more detailed explanation.'),
-        a(href='https://searchengineland.com/brainlabs-script-find-best-worst-search-queries-using-n-grams-228379', 'Even more information'),
-        p('on the power of N-grams for AdWords.')),
+      p('An n-gram is a contiguous sequence of n items from a given sequence of text or speech. You can read more', a(href='http://text-analytics101.rxnlp.com/2014/11/what-are-n-grams.html', 'here'), 'and', a(href='https://searchengineland.com/brainlabs-script-find-best-worst-search-queries-using-n-grams-228379', 'here.')),
       hr(),
       h3('Why it matters:'),
       p('This tool helps you mine your Search Terms report at a deeper level. Typically, you pull the report and aggregate terms on a phrase level. This misses a ton of opportunities. N-gram analysis helps to uncover the common phrases, and pairings, that are triggering your keywords. Ideally, you use this to find new negative and positive keywords.'),
       br(), br(),
       # Output -> Data file
-      tableOutput("contents")
+      DT::dataTableOutput("contents")
     )
   )
 )
 
 server <- function(input, output) {
 
-  output$contents <- renderTable({
+  output$contents <- DT::renderDataTable({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
     req(input$file1)
-    df <- read.csv(input$file1$datapath, header = input$header, sep = input$sep)
-    if (input$disp == "head") {
-      head(df %>%
-        dplyr::select(search.term, impr, clicks, ctr, convr, cpc) %>%
-        tidytext::unnest_tokens(ngram, search.term, token="ngrams", n=input$ngramCount) %>%
-        dplyr::group_by(ngram) %>%
-        dplyr::summarize(
-          sum.impr = sum(impr),
-          avg.impr = mean(impr),
-          sum.clicks = sum(clicks),
-          avg.clicks = mean(clicks),
-          avg.ctr = avg.clicks / avg.impr,
-          sum.convr = sum(convr),
-          avg.convr = mean(convr),
-          avg.cpc = mean(cpc)
-        ) %>%
-        dplyr::arrange(desc(sum.clicks))
-      )
-    } else {
-      head(df %>%
-        dplyr::select(search.term, impr, clicks, ctr, convr, cpc) %>%
-        tidytext::unnest_tokens(ngram, search.term, token="ngrams", n=input$ngramCount) %>%
-        dplyr::group_by(ngram) %>%
-        dplyr::summarize(
-          sum.impr = sum(impr),
-          avg.impr = mean(impr),
-          sum.clicks = sum(clicks),
-          avg.clicks = mean(clicks),
-          avg.ctr = avg.clicks / avg.impr,
-          sum.convr = sum(convr),
-          avg.convr = mean(convr),
-          avg.cpc = mean(cpc)
-        ) %>%
-        dplyr::arrange(desc(sum.clicks)), 50)
-    }
-  })
+    df  <- read_csv(input$file1$datapath)
+    names(df) %<>%
+      tolower() %>%
+      stringr::str_replace_all(" ", "")
+    DT::datatable(head(df %>%
+          dplyr::select(searchterm, impr., clicks, ctr, cost, conversions) %>%
+          tidytext::unnest_tokens(ngram, searchterm, token="ngrams", n=input$ngramCount) %>%
+          dplyr::group_by(ngram) %>%
+          dplyr::summarize(
+            sum.impr = round(sum(impr.), 2),
+            avg.impr = round(mean(impr.), 2),
+            sum.clicks = round(sum(clicks), 2),
+            avg.clicks = round(mean(clicks), 2),
+            avg.ctr = round(avg.clicks / avg.impr, 2),
+            avg.cpc = round(sum(cost) / sum(clicks), 2),
+            sum.convr = round(sum(conversions), 2),
+            avg.costperconv = round(sum.convr / sum(cost), 2)
+          ) %>%
+          dplyr::arrange(desc(sum.clicks)), 100),
+        class = "stripe hover compact order-column",
+        extensions = 'Buttons',
+        caption = 'You can sort and filter by column and export to a variety of options.',
+        options = list(
+          dom = 'Bfrtip',
+          buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+          pageLength = 50
+          ),
+        filter = 'top'
+        )
+    })
 }
+
 
 shinyApp(ui, server)
