fa924a49edb7338870c148100cffd197746c6f36
kade killary <kadekillary@protonmail.com>
2017-10-09 00:33:37 -0700

    todo: make tables -> renderDataTable

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 library(shiny)
+library(tidytext)
 library(shinythemes)
+library(tidyverse)
 
-# Define UI for data upload app
 ui <- fluidPage(theme = shinytheme("darkly"),
 
-  # App title
-  titlePanel("AdWords Ngram Analysis"),
+  titlePanel("AdWords N-gram Analysis"),
   # Sidebar layout with input and output definitions
   sidebarLayout(
     # Sidebar panel for inputs
@@ -26,32 +26,35 @@ ui <- fluidPage(theme = shinytheme("darkly"),
                                Semicolon = ";",
                                Tab = "\t"),
                    selected = ","),
-      # Input -> Select quotes
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
       # Horizontal line
       hr(),
       # Input -> Select number of rows to display
       radioButtons("disp", "Display",
                    choices = c(Head = "head",
                                Top50 = "top50"),
-                   selected = "head")
+                   selected = "head"),
+      hr(),
+      sliderInput('ngramCount', '# of Grams', min = 1, max = 6, value = 1)
     ),
     # Main panel for displaying outputs
     mainPanel(
       img(src='https://i.imgur.com/kB1qKI0.png', align = 'center'),
       br(),br(),
-      p('Explain how to pull data, clean data, then interpret results'),
+      h3('What\'s an N-gram?'),
+      p('An n-gram is a contiguous sequence of n items from a given sequence of text or speech', 
+        a(href='http://text-analytics101.rxnlp.com/2014/11/what-are-n-grams.html', ' - a more detailed explanation.'),
+        a(href='https://searchengineland.com/brainlabs-script-find-best-worst-search-queries-using-n-grams-228379', 'Even more information'),
+        p('on the power of N-grams for AdWords.')),
+      hr(),
+      h3('Why it matters:'),
+      p('This tool helps you mine your Search Terms report at a deeper level. Typically, you pull the report and aggregate terms on a phrase level. This misses a ton of opportunities. N-gram analysis helps to uncover the common phrases, and pairings, that are triggering your keywords. Ideally, you use this to find new negative and positive keywords.'),
+      br(), br(),
       # Output -> Data file
       tableOutput("contents")
     )
   )
 )
 
-# Define server logic to read selected file
 server <- function(input, output) {
 
   output$contents <- renderTable({
@@ -59,15 +62,40 @@ server <- function(input, output) {
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
     req(input$file1)
-    df <- read.csv(input$file1$datapath,
-             header = input$header,
-             sep = input$sep,
-             quote = input$quote)
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(head(df, 50))
+    df <- read.csv(input$file1$datapath, header = input$header, sep = input$sep)
+    if (input$disp == "head") {
+      head(df %>%
+        dplyr::select(search.term, impr, clicks, ctr, convr, cpc) %>%
+        tidytext::unnest_tokens(ngram, search.term, token="ngrams", n=input$ngramCount) %>%
+        dplyr::group_by(ngram) %>%
+        dplyr::summarize(
+          sum.impr = sum(impr),
+          avg.impr = mean(impr),
+          sum.clicks = sum(clicks),
+          avg.clicks = mean(clicks),
+          avg.ctr = avg.clicks / avg.impr,
+          sum.convr = sum(convr),
+          avg.convr = mean(convr),
+          avg.cpc = mean(cpc)
+        ) %>%
+        dplyr::arrange(desc(sum.clicks))
+      )
+    } else {
+      head(df %>%
+        dplyr::select(search.term, impr, clicks, ctr, convr, cpc) %>%
+        tidytext::unnest_tokens(ngram, search.term, token="ngrams", n=input$ngramCount) %>%
+        dplyr::group_by(ngram) %>%
+        dplyr::summarize(
+          sum.impr = sum(impr),
+          avg.impr = mean(impr),
+          sum.clicks = sum(clicks),
+          avg.clicks = mean(clicks),
+          avg.ctr = avg.clicks / avg.impr,
+          sum.convr = sum(convr),
+          avg.convr = mean(convr),
+          avg.cpc = mean(cpc)
+        ) %>%
+        dplyr::arrange(desc(sum.clicks)), 50)
     }
   })
 }
